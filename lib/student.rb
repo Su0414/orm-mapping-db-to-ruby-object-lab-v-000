cc84364856e24e9f7c157fa99db649ffa55b5475
Suchitra Naik <susanaik@gmail.com>
2018-05-19 21:25:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,9 @@ class Student
     sql = <<-SQL
             SELECT * FROM students WHERE grade < 12
              SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map { |row| 
+        self.new_from_db(row)
+     }
   end
 
 end
